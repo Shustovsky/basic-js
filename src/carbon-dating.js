a8f4f3d41d8e3155163fc441e3c5ea3fe1f8f2d7
A. Shustovsky <a.shustovsky@gmail.com>
2022-08-19 13:09:34 +0400

    init: carbon-dating is done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(A) {
+    if (typeof A !== 'string') {
+        return false;
+    }
+    const t = Math.ceil(Math.log((MODERN_ACTIVITY / +A)) / (0.693 / HALF_LIFE_PERIOD));
+    return t && t > 0 && t !== Infinity ? t : false;
 }
 
 module.exports = {
-  dateSample
+    dateSample
 };
