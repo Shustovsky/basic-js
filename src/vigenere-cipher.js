fdcda21bbd40022c09ad0e5e395b2d6ccbc0c40e
A. Shustovsky <a.shustovsky@gmail.com>
2022-09-12 13:17:47 +0400

    init: task is done +-

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(val) {
+        this.constructor = val;
+    }
+    encrypt(message, key) {
+        if (!message || !key) throw new Error(`Incorrect arguments!`);
+
+        const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        let result = [];
+
+
+        while (message.length > key.length) key += key;
+        key = key.toUpperCase();
+
+        let counterNotLetter = 0;
+        for (let i = 0; i < message.length; i++) {
+            let alphabetCopy = alphabet.slice();
+            let searchIndexToChange = alphabetCopy.indexOf(key[i - counterNotLetter]);
+            let cut = (alphabetCopy.splice(0, searchIndexToChange));
+            alphabetCopy = alphabetCopy.concat(cut);
+
+            let index = alphabet.indexOf(message[i].toUpperCase());
+            if (index !== -1) {
+                result.push(alphabetCopy[index]);
+            } else {
+                result.push(message[i])
+                counterNotLetter++;
+            }
+        }
+        return this.constructor === false ? result.reverse().join('') : result.join('');
+    }
+
+
+    decrypt(message, key) {
+        if (!message || !key) throw new Error(`Incorrect arguments!`);
+
+        const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+        let result = [];
+
+        while (message.length > key.length) key += key;
+        key = key.toUpperCase();
+
+        let counterNotLetter = 0;
+
+        for (let i = 0; i < message.length; i++) {
+            let alphabetCopy = alphabet.slice();
+            let searchIndexToChange = alphabetCopy.indexOf(key[i - counterNotLetter]);
+            let cut = (alphabetCopy.splice(0, searchIndexToChange));
+            alphabetCopy = alphabetCopy.concat(cut);
+            let index = alphabetCopy.indexOf(message[i].toUpperCase()); /////ПОМЕНЯЛ МЕСТАМИ
+            if (index !== -1) {
+                result.push(alphabet[index]); /////ПОМЕНЯЛ МЕСТАМИ
+            } else {
+                result.push(message[i])
+                counterNotLetter++;
+            }
+
+        }
+        return this.constructor === false ? result.reverse().join('') : result.join('');
+    }
+
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
